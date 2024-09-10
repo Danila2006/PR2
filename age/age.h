e89056f9144e976e39074a7e34b583ba27958288
Dan <118832521+Danila2006@users.noreply.github.com>
2024-09-10 10:52:16 +0300

    Delete age directory

--- age/age.h
+++
@@ -1,8 +0,0 @@
-#ifndef AGE_H
-#define AGE_H
-#include <string>
-using namespace std;
-
-string  calculateAge(int currentYear, int currentMonth, int currentDay, int birthYear, int birthMonth, int birthDay);
-
-#endif
