a65ef2a1fb749507a2d268de847e4f7d1a84043d
Dan <118832521+Danila2006@users.noreply.github.com>
2024-09-10 10:52:27 +0300

    Delete name directory

--- name/name.cpp
+++
@@ -1,5 +0,0 @@
-#include "name.h"
-
-string printName(string name) {
-    return "Hello, " + name + "!";
-}
--- name/name.h
+++
@@ -1,8 +0,0 @@
-#ifndef NAME_H
-#define NAME_H
-#include <string>
-using namespace std;
-
-string printName(string name);
-
-#endif
